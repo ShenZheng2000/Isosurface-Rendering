47b97d6559deac19f1562e0e256d5b671a2b0918
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-07-19 18:23:36 +0800

    Update explain.py

--- explain.py
+++ explain.py
@@ -42,17 +42,23 @@ get_mgrid(3, 2)
 '''
 D:\Anaconda\python.exe D:/Code/SummerResearch/critical_points/tmp_main.py
 ------------------------------------------Start------------------------------------------
+1. When side length is 3, each variable has a tensor of length 3.
 is tensor([-1.,  0.,  1.])
 shape torch.Size([3])
 ------------------------------------------tuple-----------------------------------------
+2. When we use tuple, we duplicate that tensor
 is (tensor([-1.,  0.,  1.]), tensor([-1.,  0.,  1.]))
 ------------------------------------------mgrid------------------------------------------
+3. When we use mesh grid, the first tensor is copied col by col, whereas the second vector is copied row by row.
 is (tensor([[-1., -1., -1.],
         [ 0.,  0.,  0.],
         [ 1.,  1.,  1.]]), tensor([[-1.,  0.,  1.],
         [-1.,  0.,  1.],
         [-1.,  0.,  1.]]))
 ------------------------------------------stack ------------------------------------------
+4. When we use stack, two tensors were combined as one. The mapping is (col, row, tensor) -> (row, matrix, col).
+           For example, the 1st col in the 1st row in the 1st tensor maps to the 1st row in the 1st matrix with the 1st col.
+           
 is tensor([[[-1., -1.],
          [-1.,  0.],
          [-1.,  1.]],
@@ -64,7 +70,7 @@ is tensor([[[-1., -1.],
         [[ 1., -1.],
          [ 1.,  0.],
          [ 1.,  1.]]])
-shape torch.Size([3, 3, 2])
+shape torch.Size([3, 3, 2]) # 3 tensors of size 3 (row) by 2 (col)
 ------------------------------------------reshape------------------------------------------
 is tensor([[-1., -1.],
         [-1.,  0.],
@@ -75,7 +81,7 @@ is tensor([[-1., -1.],
         [ 1., -1.],
         [ 1.,  0.],
         [ 1.,  1.]])
-shape torch.Size([9, 2])
+shape torch.Size([9, 2]) # 1 tensor of size 9 (row) by 2 (col)
 
 Process finished with exit code 0
 '''
